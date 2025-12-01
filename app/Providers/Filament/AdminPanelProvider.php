1a10be9d885758157d772b7dc54d5ef95e4f2fdd
Francis Duong <duonganhhao4751@gmail.com>
2025-12-01 21:06:52 +0700

    chore: Change name route dashboard from admin to manage.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('manage')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
